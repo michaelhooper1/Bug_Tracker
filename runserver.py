3cfa43c111f20a6116114c9acbe2e4f43a8de2f0
Michael <mhooperjnr1995@hotmail.co.uk>
2022-09-27 19:25:20 +0100

    initializing project

---
+++ runserver.py
@@ -0,0 +1,14 @@
+"""
+This script runs the Bug_tracker application using a development server.
+"""
+
+from os import environ
+from Bug_tracker import app
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
